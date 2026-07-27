b2a1acdadb347f0038c3cbe590a0bc11296cc119
31052026-neu <joshuamoore@gmx.de>
2026-07-27 17:10:21 +0200

    C Teil-1

--- aufgabe.c
+++ aufgabe.c
@@ -1,2 +1,81 @@
 #include <stdio.h>
 
+int main() 
+{
+
+
+    int A;
+    int B;
+    
+    printf("\nAND\n");                                //*Der erste Absatz zeigt an was in der Kopfzeile stehen soll//
+    printf("A | B | A AND B\n");
+    printf("----------------\n");
+
+    A = 0;                                          //*Hier gebe ich den Variablen einen Wert//
+    B = 0;
+    printf("%d | %d | %d\n", A, B, A && B);         //* Hier steht die funktion, die ausgeführt werden soll. "wenn A und B falsch sind dann muss die ausgabe auch falsch sein"
+
+    A = 0;
+    B = 1;
+    printf("%d | %d | %d\n", A, B, A && B);
+
+    A = 1;
+    B = 0;
+    printf("%d | %d | %d\n", A, B , A && B);
+
+    A = 1;
+    B = 1;
+    printf("%d | %d | %d\n", A, B, A && B);
+    
+
+    printf("\nOR\n");                                 // Hier fängt die nächste Tabelle an und zeigt mir die angegebene Kopfzeile an//
+    printf("A | B | A OR B\n");
+    printf("---------------\n");
+
+    A = 0;
+    B = 0;
+    printf ("%d | %d | %d\n", A, B, A || B);
+
+    A = 0;
+    B = 1;
+    printf("%d | %d | %d\n", A, B, A || B);
+
+    A = 1;
+    B = 0;
+    printf("%d | %d | %d\n", A, B, A || B);
+
+    A = 1;
+    B = 1;
+    printf("%d | %d | %d\n", A, B, A || B);
+
+    printf("\nXOR\n");
+    printf("A | B | A XOR B\n");
+    printf("----------------\n");
+
+    A = 0;
+    B = 0;
+    printf("%d | %d | %d\n", A, B, (A && !B) || (!A && B));
+
+    A = 0;
+    B = 1;
+    printf("%d | %d | %d\n", A, B, (A && !B) || (!A && B));
+
+    A = 1;
+    B = 0;
+    printf("%d | %d | %d\n", A, B, (A && !B) || (!A && B));
+
+    A = 1;
+    B = 1;
+    printf("%d | %d | %d\n", A, B, (A && !B) || (!A && B));
+
+    printf("\nNOT\n");
+    printf("A | B | A NOT B");
+    printf("----------------\n");
+
+    A = 0;
+    B = 0;
+    printf("%d | %d | %d %d\n", A, B, !A,  !B);
+
+     return 0;
+
+}
