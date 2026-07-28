9e0cc150ec53e693d98f283dbc0008da9c9c1d93
31052026-neu <joshuamoore@gmx.de>
2026-07-28 16:13:51 +0200

    roboter noch nicht fertig

---
+++ roboter.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int zustand = 1;
+    int befehl;
+
+    printf("Gib eine Zahl ein um einen Befehl durchzuführen!: \n");
+    scanf("%d", &befehl);
+    
+    if ()
+
+
+}
