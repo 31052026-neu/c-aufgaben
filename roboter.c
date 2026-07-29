b1a17b68ca6a0b739f0bf1ae302a726d96931d7e
31052026-neu <joshuamoore@gmx.de>
2026-07-29 09:09:34 +0200

    Roboter_v1

--- roboter.c
+++ roboter.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    int zustand = 1;
+    int glücklich = 1;
+    int traurig = 2;
+    int müde = 3;
+    int verwirrt = 4;
+    int zustand = traurig;
     int befehl;
 
     printf("Gib eine Zahl ein um einen Befehl durchzuführen!: \n");
     scanf("%d", &befehl);
+
+    printf("Der Roboter ist glücklich.\n");
     
-    if ()
+    if (befehl == 1) {
+        printf(" Der Roboter blinkt fröhlich!\n");
+    }
+    else if (befehl == 2) {
+        printf("Der Roboter dreht sich!\n");
+    }
+    else if (befehl == 3) {
+        printf("Der Roboter hupt!\n");
+    }
+    else {
+        printf("Befehl ist ungültig!\n");
+    }
 
-
+    return 0;
 }
