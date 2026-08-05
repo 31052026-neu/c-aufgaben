4e54a7027c08b4e75393119c9b69f3a95e2c7211
31052026-neu <joshuamoore@gmx.de>
2026-08-05 08:28:19 +0200

    afugabe 4 fertig2

--- aufgabe_4.c
+++ aufgabe_4.c
@@ -1,37 +1,38 @@
 
 #include <stdio.h>
 
-int main () {
-    
-    int matrix[4][4] = {
-        {2,  3,  5,   7},
-        {11, 13, 17, 19},
-        {23, 29, 31, 37},
-        {41, 43, 47, 53},
-    };
-    for ( int i = 0; i < 4; i++) {
-        int summe = 0;
-        for (int j = 0; j < 4; j++) {
-            summe += matrix [i][j];
-      }
-      printf ("Die Zeile %d hat die Summe %d\n",i ,summe);
-    }
-    for ( int i = 0; i < 4; i++) {
-        int summe = 0;
-        for (int j = 0; j < 4; j++) {
-            summe += matrix [j][i];
-        }
-        printf ("Die Spalte %d hat die Summe %d\n", i, summe);
-    }
+int main() {
+
+  int matrix[4][4] = {
+      {2, 3, 5, 7},
+      {11, 13, 17, 19},
+      {23, 29, 31, 37},
+      {41, 43, 47, 53},
+  };
+  for (int i = 0; i < 4; i++) {
     int summe = 0;
-    for (int i = 0; i <4; i++) {
-        summe += matrix [i][i];
+    for (int j = 0; j < 4; j++) {
+      summe += matrix[i][j];
     }
-    printf ("Die Diagonale von oben links hat die Summe %d\n", summe);
-    
-        summe = 0;
-    for (int i = 0; i < 4; i++) {
-        summe +=matrix [i][3 - i];
+    printf("Die Zeile %d hat die Summe %d\n", i, summe);
+  }
+  for (int i = 0; i < 4; i++) {
+    int summe = 0;
+    for (int j = 0; j < 4; j++) {
+      summe += matrix[j][i];
     }
-    printf("Die Diagonale von oben rechts hat die Summe %d\n", summe);
-    return 0;
+    printf("Die Spalte %d hat die Summe %d\n", i, summe);
+  }
+  int summe = 0;
+  for (int i = 0; i < 4; i++) {
+    summe += matrix[i][i];
+  }
+  printf("Die Diagonale von oben links hat die Summe %d\n", summe);
+
+  summe = 0;
+  for (int i = 0; i < 4; i++) {
+    summe += matrix[i][3 - i];
+  }
+  printf("Die Diagonale von oben rechts hat die Summe %d\n", summe);
+  return 0;
+}
